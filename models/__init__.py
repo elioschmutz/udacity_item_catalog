22f15e18e26eaae11ff37be2558f5dc3877f5756
Elio Schmutz <e.schmutz@4teamwork.ch>
2019-01-02 22:45:37 +0100

    feat: add category-model

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,20 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
 from sqlalchemy import create_engine
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(80), nullable=False)
+    items = relationship("Item", backref="category")
 
 
 engine = create_engine('sqlite:///item_catalog.db')
