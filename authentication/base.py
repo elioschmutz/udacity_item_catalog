1486649b6135d0c5baba1e08afb0e06754df7390
Elio Schmutz <e.schmutz@4teamwork.ch>
2019-01-10 16:04:29 +0100

    refactor: use baseclass for auth providers

---
+++ authentication/base.py
@@ -0,0 +1,69 @@
+from flask import session as flask_session
+from models import LoginSession
+from models import User
+import json
+
+
+class UserData(object):
+    def __init__(self, email, name, img_url):
+        self.email = email
+        self.name = name
+        self.img_url = img_url
+
+
+class AuthProvider(object):
+
+    secrets_file_name = None
+    secrets = None
+    provider_name = None
+
+    def __init__(self):
+        self.secrets = self.load_secrets()
+
+    def login(self, code):
+        """Performs all the required steps to login a user
+        """
+        access_token, userdata = self.process_auth_provider_login(code)
+        return self.process_local_login(access_token, userdata)
+
+    def logout(self, token=''):
+        pass
+
+    def process_auth_provider_login(self, code):
+        """Is doing everything needed to validate the auth code, fetching an
+        access token and user-infos.
+
+        This function should return the access_token and a userdata object.
+        """
+        raise NotImplementedError
+
+    def process_local_login(self, access_token, userdata):
+        """After validating the tokens and fetching the user-infos from the
+        auth-provider, it is required to create/get a local user-object
+        and safe the access-token for further requests into a session object.
+        """
+        user = self.get_or_create_user(userdata)
+
+        # Create a new login-session
+        flask_session['access_token'] = access_token
+        return LoginSession.create(
+            token=access_token,
+            provider=self.provider_name,
+            user=user)
+
+    def get_or_create_user(self, userdata):
+        """Returns a user-instance.
+
+        If a user with the given email already exists, it just returns the given
+        one. Otherwise, it creates a new user.
+        """
+        user = User.lookup_by_email(userdata.email)
+        if not user:
+            user = User.create(name=userdata.name,
+                               email=userdata.email,
+                               picture=userdata.img_url)
+        return user
+
+    def load_secrets(self):
+        with open(self.secrets_file_name, 'r') as file_:
+            return json.loads(file_.read())
--- authentication/google.py
+++ authentication/google.py
@@ -1,33 +1,30 @@
+from authentication.base import AuthProvider
+from authentication.base import UserData
 from authentication.errors import AccessTokenValidationError
-from flask import session as flask_session
-from models import LoginSession
-from models import User
 from oauth2client.client import flow_from_clientsecrets
-import json
 import requests
 
 
-class GoogleAuth(object):
-    error = None
-    secrets = None
+class GoogleAuth(AuthProvider):
     oauth_endpoint = 'https://www.googleapis.com/oauth2/v1'
     secrets_file_name = 'google_secrets.json'
+    provider_name = 'google'
 
-    def __init__(self):
-        self.secrets = self._load_secrets()
-
-    def login(self, auth_code):
+    def process_auth_provider_login(self, code):
         """Logs in a user based on the auth_code received from the frontend
         """
-        credentials = self._get_credentials_object(auth_code)
-        tokeninfo = self._get_token_info(credentials.access_token)
+        credentials = self._get_credentials_object(code)
+        access_token = credentials.access_token
 
-        self._validate_access_token(credentials, tokeninfo)
-        return self._process_login(credentials)
+        self._validate_access_token(credentials)
+
+        userdata = self._lookup_user_info(access_token)
+
+        return access_token, userdata
 
     def logout(self, access_token):
         requests.post('https://accounts.google.com/o/oauth2/revoke',
-                      params={'token':access_token},
+                      params={'token': access_token},
                       headers={'content-type': 'application/x-www-form-urlencoded'})
 
     def _get_credentials_object(self, code):
@@ -50,10 +47,11 @@ class GoogleAuth(object):
 
         return result
 
-    def _validate_access_token(self, credentials, tokeninfo):
+    def _validate_access_token(self, credentials):
         """Valdiate the accesstoken
         """
         # Verify that the access token is used for the intended user.
+        tokeninfo = self._get_token_info(credentials.access_token)
         gplus_id = credentials.id_token['sub']
         if tokeninfo['user_id'] != gplus_id:
             raise AccessTokenValidationError(
@@ -64,32 +62,12 @@ class GoogleAuth(object):
             raise AccessTokenValidationError(
                 "Token's client ID does not match app's.")
 
-    def _process_login(self, credentials):
-        userinfo = self._lookup_user_info(credentials.access_token)
-        user = self._get_or_create_user(userinfo)
-
-        # Create a new login-session
-        flask_session['access_token'] = credentials.access_token
-        return LoginSession.create(
-            token=credentials.access_token,
-            provider='google',
-            user=user)
-
     def _lookup_user_info(self, access_token):
-        # Get user info
         userinfo_url = "{}/userinfo".format(self.oauth_endpoint)
         params = {'access_token': access_token, 'alt': 'json'}
-        return requests.get(userinfo_url, params=params).json()
+        userinfos = requests.get(userinfo_url, params=params).json()
 
-    def _get_or_create_user(self, userinfo):
-        # see if user exists, if it doesn't make a new one
-        user = User.lookup_by_email(userinfo.get('email'))
-        if not user:
-            user = User.create(name=userinfo.get('name', ''),
-                               email=userinfo.get('email'),
-                               picture=userinfo.get('picture', ''))
-        return user
-
-    def _load_secrets(self):
-        with open(self.secrets_file_name, 'r') as file_:
-            return json.loads(file_.read())
+        return UserData(
+            userinfos.get('email'),
+            userinfos.get('name'),
+            userinfos.get('picture'))
